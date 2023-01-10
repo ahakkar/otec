ab32908648b56a036323638005d8495db07581c7
Antti Hakkarainen <a.hakkarainen@iki.fi>
2023-01-10 16:27:16 +0200

    juuret solution

--- h1/juuret/juuret.c
+++ h1/juuret/juuret.c
@@ -1,7 +1,30 @@
+/*
+ * COMP.CS.120 Ohjelmoinnin Tekniikka C
+ * H1 T7 juuret
+ *
+ * Antti Hakkarainen
+ * K79735 vqanha
+ * antti.i.hakkarainen@tuni.fi
+ */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
 
-int main()
-{
-    printf("Hello World!\n");
+int EXTRA_ARGS = 2;
+
+int main(int argc, char *argv[]) {
+
+    int prec = atof(argv[1]);
+    int i = 0;
+
+    while (i < argc-EXTRA_ARGS) {
+        printf("sqrt(%.*f) = %.*f\n",
+               prec, atof(argv[i+EXTRA_ARGS]),
+               prec, sqrt(atof(argv[i+EXTRA_ARGS])));
+        i++;
+    }
     return 0;
+
 }
